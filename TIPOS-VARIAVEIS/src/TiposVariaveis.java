0c943893353472151f5defa541a2848e387a60af
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-04-26 16:23:20 -0300

    Tipos e Variáveis

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+
+        
     }
 }
+//byte idade = 123;
+//short ano = 2024;
+//int cep = 21070333; (se começar com zero usar string)
+//long cpf = 9876543219L;(se começar com zero usar string) (precisa encerrar com L)
+//float pi = 3.14F; (precisa encerrar com F)
+//double salario = 1275.33;
+//final o valor não será mudado;
