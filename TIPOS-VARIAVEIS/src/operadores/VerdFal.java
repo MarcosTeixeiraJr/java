3ac1344556ed2f8bb58181fe5a0a3d20f6929da6
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-04-26 17:51:16 -0300

    Verdadeiro ou Falso

--- TIPOS-VARIAVEIS/src/operadores/VerdFal.java
+++ TIPOS-VARIAVEIS/src/operadores/VerdFal.java
@@ -1,5 +1,22 @@
 package operadores;
 
 public class VerdFal {
-    
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        /*String resultado = "";
+        if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";*/
+
+        /*String resultado = a == b ? "verdadeiro" : "false";*/
+
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+    }
 }
