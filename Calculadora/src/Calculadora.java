d73092f4fec4830ffee865a65cf867009913b4f5
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-05-01 13:07:23 -0300

    Calculadora

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Marcos Teixeira
+ * @version 1.0
+ * @since 01/05/2024
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro dentro do método
+     * @param numeroDois este é o segundo parâmetro dentro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar (int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ Calculadora/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha 
+
+        /*
+        * Olá,
+        * eu sou um comentário
+        * que posso ser mais detalhadod
+        * quando necessário
+        */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     * Que incrível !!!
+     */
+    public void metodo(){
+
+    }
+}
