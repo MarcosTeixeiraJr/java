62281099d707b5c02912e2648b79ac6c314c824b
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-05-07 14:33:23 -0300

    Controle de saque

--- Simulação-Banco/src/ControleSimplesDeSaques.java
+++ Simulação-Banco/src/ControleSimplesDeSaques.java
@@ -3,44 +3,37 @@ import java.util.Scanner;
 public class ControleSimplesDeSaques {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in); 
+        
         double limiteDiario = scanner.nextDouble();
-        double primeiroSaque = scanner.nextDouble();
-        double saldoRestante = limiteDiario - primeiroSaque;
-
-        // Realiza o primeiro saque
-        if (primeiroSaque <= limiteDiario) {
-            System.out.println("Saque realizado. Limite restante: " + saldoRestante);
-        } else {
-            System.out.println("Limite diário de saque atingido. Transações encerradas.");
-            scanner.close();
-            
+        double saldoDisponivel = limiteDiario;
+        
+        for ( ; ; ){
+          double valorSaque = scanner.nextDouble();
+          
+          if (valorSaque == 0) {
+            System.out.println("Transacoes encerradas.");
+            break;
+          }
+          
+          if (valorSaque <= saldoDisponivel) {
+            saldoDisponivel -= valorSaque;
+            System.out.println("Saque realizado. Limite restante: " + saldoDisponivel);
+          }else{
+            System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+            break;
+          }
+          
         }
-
-        // Itera sobre os saques adicionais
-        for (int i = 1; ; i++) {
-            double valorSaque = scanner.nextDouble();
-
-            if (valorSaque == 0) {
-                System.out.println("Transações encerradas.");
-                break;
-            }
-
-            if (valorSaque > limiteDiario) {
-                System.out.println("Limite diário de saque atingido. Transações encerradas.");
-                break;
-            }
-
-            if (valorSaque > saldoRestante) {
-                System.out.println("Saldo insuficiente. Transações encerradas.");
-                break;
-            }
-
-            saldoRestante -= valorSaque;
-            System.out.println("Saque realizado. Limite restante: " + saldoRestante);
-        }
-
         scanner.close();
-    }
+    }   
 }
+            
+            
+           
+           
+            
+           
+            
+     
+            
