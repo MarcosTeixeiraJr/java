b3288820de297791054f80e54d0701150a50024e
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-05-09 15:38:15 -0300

    Pacotes

--- Exemplo-Pacotes/Pacotes/src/escola/Aluno.java
+++ Exemplo-Pacotes/Pacotes/src/escola/Aluno.java
@@ -1,6 +1,19 @@
 package escola;
 
 public class Aluno {
-    String nome;
-    int idade;
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String newNome) {
+        nome = newNome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int newIdade) {
+        this.idade = newIdade;
+    }
 }
