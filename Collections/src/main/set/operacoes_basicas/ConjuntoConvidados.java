dd63fbfa1044da414aa66ea1fbbddc713fcc9bcc
Marcos051 <157824330+MarcosTeixeiraJr@users.noreply.github.com>
2024-05-16 17:05:43 -0300

    Contatos

--- Collections/src/main/set/operacoes_basicas/ConjuntoConvidados.java
+++ Collections/src/main/set/operacoes_basicas/ConjuntoConvidados.java
@@ -4,54 +4,66 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ConjuntoConvidados {
-    //atributo
-
+    // Atributo para armazenar o conjunto de convidados
     private final Set<Convidado> convidadoSet;
 
+    // Construtor para inicializar o conjunto de convidados como um HashSet vazio
     public ConjuntoConvidados() {
         this.convidadoSet = new HashSet<>();
     }
 
+    // Método para adicionar um novo convidado ao conjunto
     public void adicionarConvidado(String nome, int codigoConvite) {
         convidadoSet.add(new Convidado(nome, codigoConvite));
     }
 
+    // Método para remover um convidado com base no código de convite
     public void removerCovidadoPorCodigoConvite(int codigoConvite) {
         Convidado convidadoParaRemover = null;
+        // Procura pelo convidado com o código de convite especificado
         for(Convidado c : convidadoSet) {
             if(c.getCodigoConvite() == codigoConvite) {
                 convidadoParaRemover = c;
-                break;
+                break; // Uma vez que o convidado é encontrado, não é necessário continuar iterando
             }
-            
         }
+        // Remove o convidado do conjunto
         convidadoSet.remove(convidadoParaRemover);
     }
 
+    // Método para contar quantos convidados estão presentes no conjunto
     public int contarConvidados() {
         return convidadoSet.size();
     }
 
+    // Método para exibir os convidados presentes no conjunto
     public void exibirConvidados() {
         System.out.println(convidadoSet);
     }
 
+    // Método main para testar a funcionalidade da classe
     public static void main(String[] args) {
+        // Cria uma instância da classe ConjuntoConvidados
         ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        // Exibe quantos convidados estão presentes no conjunto (deve ser 0 inicialmente)
         System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de convidados");
 
-
+        // Adiciona alguns convidados ao conjunto
         conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
         conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
-        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235); // Esse código de convite já existe
         conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
         conjuntoConvidados.adicionarConvidado("Convidado 5", 1237);
 
+        // Exibe quantos convidados estão presentes no conjunto após a adição
         System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de convidados");
 
+        // Remove um convidado do conjunto com base no código de convite
         conjuntoConvidados.removerCovidadoPorCodigoConvite(1234);
-        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de //convidados");
+        // Exibe quantos convidados estão presentes no conjunto após a remoção
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de convidados");
 
+        // Exibe os convidados presentes no conjunto
         conjuntoConvidados.exibirConvidados();
     }
 }
--- Collections/src/main/set/operacoes_basicas/Convidado.java
+++ Collections/src/main/set/operacoes_basicas/Convidado.java
@@ -1,39 +1,43 @@
 package main.set.operacoes_basicas;
 
 public class Convidado {
-    //atributo
-
+    // Atributos
     private final String nome;
-
     private final int codigoConvite;
 
-    public Convidado (String nome, int codigoConvite) {
+    // Construtor
+    public Convidado(String nome, int codigoConvite) {
         this.nome = nome;
         this.codigoConvite = codigoConvite;
     }
 
+    // Método para obter o nome do convidado
     public String getNome() {
         return nome;
     }
 
+    // Método para obter o código de convite do convidado
     public int getCodigoConvite() {
         return codigoConvite;
     }
 
-    /*@Override
+    // Sobrescrevendo o método equals para comparar convidados com base no código de convite
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Convidado convidado)) return false;
         return getCodigoConvite() == convidado.getCodigoConvite();
     }
 
+    // Sobrescrevendo o método hashCode para calcular o hash com base no código de convite
     @Override
     public int hashCode() {
-        return Object.hash(getCodigoConvite());
-    }*/
+        return codigoConvite; // Retorna o código de convite como hash
+    }
 
+    // Sobrescrevendo o método toString para obter uma representação de string do objeto Convidado
     @Override
-    public String toString(){
+    public String toString() {
         return "Convidado{" +
             "nome='" + nome + '\'' +
             ", codigoConvite=" + codigoConvite +
